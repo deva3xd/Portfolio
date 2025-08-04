6f07b911bd6558d18da38c4680b66186c6657195
deva3xd <devarn05@gmail.com>
2025-08-04 23:35:41 +0700

    delete some project

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,9 +3,7 @@ import React, { useEffect } from "react";
 import Aos from "aos";
 import Github from "../assets/icons/github.png";
 import Movieflix from "../assets/images/movieflix.png";
-import Jms from "../assets/images/jms.png";
 import Beatwave from "../assets/images/beatwave.png";
-import Advertising from "../assets/images/advertising.png";
 
 const Project = () => {
     useEffect(() => {
@@ -19,32 +17,6 @@ const Project = () => {
                     <h2 className="text-3xl sm:text-6xl font-bold mb-2 sm:mb-5">Project</h2>
                 </div>
                 <div className="flex flex-wrap justify-center gap-5 px-10 lg:px-0">
-                    <div className="lg:max-w-md bg-white border border-white rounded-sm shadow">
-                        <span className="h-full">
-                            <img className="lg:h-60" src={Advertising} alt="advertising" />
-                        </span>
-                        <div className="p-2">
-                            <span className='flex justify-between items-center'>
-                                <h5 className="text-lg sm:text-xl font-medium tracking-tight text-custom-tertiary">Advertising</h5>
-                                <a href="https://github.com/deva3xd/Advertising.git" className='text-white bg-black text-xs flex items-center py-2 px-3 rounded-lg gap-1'>
-                                    <img src={Github} alt='Github' width={15} />
-                                    Source</a>
-                            </span>
-                        </div>
-                    </div>
-                    <div className="lg:max-w-md bg-white border border-white rounded-sm shadow">
-                        <span className="h-full">
-                            <img className="lg:h-60" src={Jms} alt="jms" />
-                        </span>
-                        <div className="p-2">
-                            <span className='flex justify-between items-center'>
-                                <h5 className="text-lg sm:text-xl font-medium tracking-tight text-custom-tertiary">Job Management System</h5>
-                                <a href="https://github.com/deva3xd/Capstone-Project.git" className='text-white bg-black text-xs flex items-center py-2 px-3 rounded-lg gap-1'>
-                                    <img src={Github} alt='Github' width={15} />
-                                    Source</a>
-                            </span>
-                        </div>
-                    </div>
                     <div className="lg:max-w-md bg-white border border-white rounded-sm shadow">
                         <span className="h-full">
                             <img className="lg:h-60" src={Movieflix} alt="movieflix" />
