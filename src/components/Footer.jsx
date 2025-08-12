22a15776c05511cc587a10fe3c85dd24dabe9b39
deva3xd <devarn05@gmail.com>
2025-08-12 23:43:05 +0700

    ui(footer): remove vertical padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { SiGithub } from 'react-icons/si';
 
 const Footer = () => {
     return (
-        <footer className="border-t border-primary px-5 sm:px-20 py-2 flex flex-row items-center justify-between">
+        <footer className="border-t border-primary px-5 sm:px-20 flex flex-row items-center justify-between">
             <a href="https://github.com/deva3xd" className="bg-primary p-2">
                 <SiGithub size={30} className='text-default' />
             </a>
