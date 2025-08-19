f3e7cf52f15efda33994cce3b860759ceebb752c
deva3xd <devarn05@gmail.com>
2025-08-19 23:38:59 +0700

    update project section

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Movieflix from "../images/movieflix.png";
 import Beatwave from "../images/beatwave.png";
 import YourTask from "../images/yourtask.png";
-import { SiLaravel, SiInertia, SiTailwindcss, SiMysql, SiShadcnui, SiDaisyui, SiVite, SiNextdotjs } from "react-icons/si";
+import { SiLaravel, SiInertia, SiTailwindcss, SiMysql, SiShadcnui, SiNextdotjs, SiPrisma } from "react-icons/si";
 
 const Project = () => {
     return (
@@ -21,16 +21,16 @@ const Project = () => {
                             <div className="bg-default text-primary px-1 rounded-sm">progress</div>
                         </div>
                         <div className="flex flex-row items-center gap-1">
-                            <div className="bg-default p-1 border border-primary rounded-sm">
+                            <div className="bg-default p-1 border border-default rounded-sm">
                                 <SiLaravel className="text-primary" />
                             </div>
-                            <div className="bg-default p-1 border border-primary rounded-sm">
+                            <div className="bg-default p-1 border border-default rounded-sm">
                                 <SiInertia className="text-primary" />
                             </div>
-                            <div className="bg-default p-1 border border-primary rounded-sm">
-                                <SiDaisyui className="text-primary" />
+                            <div className="bg-default p-1 border border-default rounded-sm">
+                                <SiTailwindcss className="text-primary" />
                             </div>
-                            <div className="bg-default p-1 border border-primary rounded-sm">
+                            <div className="bg-default p-1 border border-default rounded-sm">
                                 <SiMysql className="text-primary" />
                             </div>
                         </div>
@@ -47,13 +47,16 @@ const Project = () => {
                         </div>
                         <div className="flex flex-row items-center gap-1">
                             <div className="bg-primary p-1 border border-primary rounded-sm">
-                                <SiVite className="text-default" />
-                            </div>
-                            <div className="bg-primary p-1 border border-primary rounded-sm">
-                                <SiTailwindcss className="text-default" />
+                                <SiNextdotjs className="text-default" />
                             </div>
                             <div className="bg-primary p-1 border border-primary rounded-sm">
                                 <SiShadcnui className="text-default" />
+                            </div>
+                            <div className="bg-primary p-1 border border-primary rounded-sm">
+                                <SiPrisma className="text-default" />
+                            </div>
+                            <div className="bg-primary p-1 border border-primary rounded-sm">
+                                <SiMysql className="text-default" />
                             </div>
                         </div>
                     </div>
@@ -74,20 +77,26 @@ const Project = () => {
                             <div className="bg-primary p-1 border border-primary rounded-sm">
                                 <SiTailwindcss className="text-default" />
                             </div>
+                            <div className="bg-primary p-1 border border-primary rounded-sm">
+                                <SiPrisma className="text-default" />
+                            </div>
+                            <div className="bg-primary p-1 border border-primary rounded-sm">
+                                <SiMysql className="text-default" />
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="bg-primary text-default p-4 border-2 border-primary order-3 sm:order-4">
-                    <p className="underline font-medium text-xl sm:text-2xl">--</p>
-                    <p className="text-lg font-light">--</p>
-                    <img className="my-2 border border-primary h-auto sm:h-52" src={Movieflix} alt="movieflix" />
+                   <p className="underline font-medium text-xl sm:text-2xl">Upcoming</p>
+                    <p className="text-lg font-light">???</p>
+                    <img className="my-2 border border-primary h-auto sm:h-52" src={Movieflix} alt="MovieFlix" />
                     <div className="flex flex-row justify-between">
-                        <div className="flex flex-row items-center w-1/2">
-                            <a href="#" className="hover:underline">github</a> |
+                        <div className="flex flex-row items-center">
+                            <a href="https://github.com/deva3xd/Movie-App.git" className="hover:underline">github</a> |
                             <div className="bg-default text-primary px-1 rounded-sm">progress</div>
                         </div>
                         <div className="flex flex-row items-center gap-1">
-                            <div className="bg-default p-1 border border-primary rounded-sm">
+                            <div className="bg-default p-1 border border-default rounded-sm">
                                 <SiLaravel className="text-primary" />
                             </div>
                         </div>
