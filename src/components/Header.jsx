f09759a25da448a2cdb2390613347cb15bdbd414
deva3xd <devarn05@gmail.com>
2025-12-25 20:38:39 +0700

    fix typo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
                 <div className="w-full md:w-3/5 text-default flex flex-col justify-center">
                     <span className="font-medium text-base lg:text-2xl">Hi, I'm Deva Rahma Nugroho</span>
                     <h1 className="font-bold text-2xl lg:text-5xl">Full-Stack Web Developer</h1>
-                    <p className="font-light text-xs lg:text-base">I live in Indonesia, has more than 2 years experience in building websites. I'm a responsible person, so the project I make will not exceed the specified tile limit.</p>
+                    <p className="font-light text-xs lg:text-base">I live in Indonesia, has more than 2 years experience in building websites. I'm a responsible person, so project what I make will not exceed the specified time limit.</p>
                 </div>
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-4 mt-2 gap-2">
